22d93c3d1750b288572990e4a803c3cf583bd33c
Carlos Eduardo Fernandes <dufernandess@hotmail.com>
2024-02-07 12:38:25 -0300

    Finalizando responsividade e javascript tela Fale conosco

--- script_js/fale_conosco.js
+++ script_js/fale_conosco.js
@@ -44,3 +44,55 @@ manutencaoPopup(nomeInput, nomeSpan);
 manutencaoPopup(emailInput, emailSpan);
 manutencaoPopup(textareaInput, textareaSpan);
 
+//validando campo nome:
+nomeInput.addEventListener("change", (e) => {
+    let nome = e.target.value;
+    if (nome.length > 5) {
+        nomeSpan.innerHTML = "✓";
+        nomeSpan.style.color = "#FF0096";
+        nomeSpan.style.fontSize = "20px";
+    } else if (nome.length == 0){
+        nomeHelper.classList.remove("visivel");
+        nomeSpan.innerHTML = "";
+    } else {
+        nomeHelper.classList.add("visivel");
+        nomeHelper.innerHTML = "Você deve inserir, no mínimo, 5 caracteres";
+        nomeSpan.innerHTML = "";
+    }
+})
+
+//validando campo email:
+emailInput.addEventListener("change", (e) => {
+    let email = e.target.value;
+    if (email.includes("@") && email.includes(".com")) {
+        emailSpan.innerHTML = "✓";
+        emailSpan.style.color = "#FF0096";
+        emailSpan.style.fontSize = "20px";
+    } else if (email.length == 0){
+        emailHelper.classList.remove("visivel");
+        emailSpan.innerHTML = "";
+    }else {
+        emailHelper.classList.add("visivel");
+        emailHelper.innerHTML = "Digite um e-mail válido (deve conter @ e .com)";
+        emailSpan.innerHTML = "";
+    }
+})
+
+//validando campo textarea:
+textareaInput.addEventListener("change", (e) => {
+    let textarea = e.target.value;
+    if (textarea.length > 15) {
+        textareaSpan.innerHTML = "✓";
+        textareaSpan.style.color = "#FF0096";
+        textareaSpan.style.fontSize = "20px";
+        textareaSpan.style.marginLeft = "25px";
+    } else if (textarea.length == 0){
+        textareaHelper.classList.remove("visivel");
+        textareaSpan.innerHTML = "";
+    } else {
+        textareaHelper.classList.add("visivel");
+        textareaHelper.innerHTML = "Você deve inserir, no mínimo, 15 caracteres";
+        textareaSpan.innerHTML = "";
+    }
+})
+
